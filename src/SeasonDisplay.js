9dee5519fa998f198effbbc7fed24d80633d7e95
Thales Avila <thalespizza@gmail.com>
2019-03-13 13:16:38 -0300

    Implementando Season Display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Diplay</div>;
+const SeasonDisplay = props => {
+  console.log(props);
+
+  return <div>Latitute: {props.lat}</div>;
 };
 
 export default SeasonDisplay;
